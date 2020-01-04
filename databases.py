b96c3a4f90d11c0fc94ac4dca04dfe32e68edeb3
Kaloyan Angelov <kaloyan.angelov.tues@gmail.com>
2020-01-04 16:16:33 +0200

    [database] [init - created them]

---
+++ databases.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+DB_NAME = 'example.db'
+
+conn = sqlite3.connect(DB_NAME)
+
+conn.cursor().execute('''
+CREATE TABLE IF NOT EXISTS products
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        content TEXT,
+        price REAL,
+        published TIMESTAMP,
+        is_active INTEGER NOT NULL,
+        owner_id INTEGER,
+        publisher_id INTEGER,
+        FOREIGN KEY(owner_id) REFERENCES users(id)
+        FOREIGN KEY(publisher_id) REFERENCES users(id)
+    )
+''')
+conn.cursor().execute('''
+CREATE TABLE IF NOT EXISTS users
+    (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+''')
+conn.commit()
+
+
+class DB:
+    def __enter__(self):
+        self.conn = sqlite3.connect(DB_NAME)
+        return self.conn.cursor()
+
+    def __exit__(self, type, value, traceback):
+        self.conn.commit()
